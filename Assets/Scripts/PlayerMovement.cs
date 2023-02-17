63ee5082878db2d9cf763f9b02fb873a88613195
dirdr <adrien.pelfresne@gmail.com>
2023-02-17 22:21:08 +0100

    fixing ground and added first primitive movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float moveSpeed;
+    public float velocity;
     Rigidbody rb;
-    Vector3 vel;
-    Vector3 rot;
+    private Vector3 movement;
+ 
 
     private void Awake()
     {
@@ -17,15 +17,13 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        float xInput = Input.GetAxis("Horizontal");
-        float yInput = Input.GetAxis("Vertical");
-        vel = new Vector3(xInput, 0, yInput).normalized;
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float zInput = Input.GetAxisRaw("Vertical");
+        movement = transform.forward * zInput + transform.right * xInput;
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-
-        //rb.AddForce(vel , ForceMode.Force);
-        rb.velocity = new Vector3(vel.x * moveSpeed, 0, vel.z * moveSpeed);
+        rb.AddForce(movement * velocity, ForceMode.Force);
     }
 }

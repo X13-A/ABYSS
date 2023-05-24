ea9beefea21d5deb5631eb885c1251ce63117e97
adrienpelfresne <adrien.pelfresne@edu.esiee.fr>
2023-05-24 11:36:29 +0200

    playerSearcher update

--- Assets/Scripts/Enemies/Cactus/CactusAnimationController.cs
+++ Assets/Scripts/Enemies/Cactus/CactusAnimationController.cs
@@ -1,27 +1,39 @@
+using SDD.Events;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CactusAnimationController : MonoBehaviour
+public class CactusAnimationController : MonoBehaviour 
 {
     private Animator m_Animator;
+    private Rigidbody m_Rigidbody;
+    private PlayerSearcher m_PlayerSearcher;
 
     [SerializeField] private AnimationClip walk;
     [SerializeField] private AnimationClip run;
 
+    private float runningWhenModifier = 1.3f;
 
     private void Awake()
     {
         m_Animator = GetComponent<Animator>();
-        
-    }
-    void Start()
-    {
-        
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_PlayerSearcher = GetComponent<PlayerSearcher>();
     }
 
-    void Update()
+    private void Update()
     {
-        
+        Vector3 velocity = m_Rigidbody.velocity;
+        bool hasMovement = velocity.x > float.Epsilon;
+        float distanceToPlayer = m_Animator.GetFloat("distanceToPlayer");
+        float detectionRadius = m_PlayerSearcher.detectionRadius;
+        if (hasMovement && distanceToPlayer >= detectionRadius / runningWhenModifier && distanceToPlayer <= detectionRadius) // running
+        {
+            PlayerSearcher.velocity = 
+            m_Animator.SetBool("isWalking", true);
+        }
+        else if (hasMovement && distanceToPlayer < detectionRadius / runningWhenModifier && distanceToPlayer > 0) { // walking
+            m_Animator.SetBool("isRunning", true);
+    	}
     }
 }
--- Assets/Scripts/Enemies/PlayerSearcher.cs
+++ Assets/Scripts/Enemies/PlayerSearcher.cs
@@ -1,23 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerSearcher : MonoBehaviour
 {
-    [SerializeField] private float detectionRadius;
-    [SerializeField] private float velocity;
+    private Animator m_Animator;
+    private Rigidbody m_Rigidbody;
+
+    public float detectionRadius;
+    [SerializeField] private float runningVelocity;
+    [SerializeField] private float walkingVelocity;
+    [SerializeField] private float runningWhenModifier;
     [SerializeField] private Transform playerReference;
 
-
     private float distanceToPlayer;
+    private bool isWalking;
+    private bool isRunning;
 
-    private void Start()
+    private void Awake()
     {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
         SearchPlayer();
+        UpdateAnimator();
     }
 
     private void SearchPlayer()
@@ -27,15 +34,43 @@ public class PlayerSearcher : MonoBehaviour
             return;
         }
         distanceToPlayer = Vector3.Distance(transform.position, playerReference.position);
+        if (distanceToPlayer <= detectionRadius && distanceToPlayer > detectionRadius / runningWhenModifier)
+	    {
+            isWalking = true;
+            isRunning = false;
+	    } else if (distanceToPlayer > 0 && distanceToPlayer <= detectionRadius / runningWhenModifier) 
+	    {
+            isRunning = true;
+            isWalking = false;
+	    }
+    }
+
+    private void UpdateAnimator()
+    {
+        m_Animator.SetFloat("distanceToPlayer", distanceToPlayer);
+        m_Animator.SetBool("isWalking", isWalking);
+        m_Animator.SetBool("isRunning", isRunning);
     }
 
     private void FixedUpdate()
-            {
+    {
         if (distanceToPlayer <= detectionRadius)
         {
             Vector3 directionToPlayer = (playerReference.position - transform.position).normalized;
-            transform.LookAt(playerReference.transform);
-            transform.position += directionToPlayer * velocity;
+            m_Rigidbody.MoveRotation(Quaternion.LookRotation(directionToPlayer));
+            m_Rigidbody.MovePosition(transform.position + directionToPlayer * GetCurrentVelocity());
         }
+    }
+
+    private float GetCurrentVelocity()
+    {
+        if (isWalking && !isRunning) 
+	    {
+            return walkingVelocity;
+	    } else if (isRunning && !isWalking)
+	    {
+            return runningVelocity; 
+	    }
+        return 0f;
     }
 }
